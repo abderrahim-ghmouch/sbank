cd9f00826a95a543e91f81ea90c74c011411b38a
abderrahim ghmouch <abderrahimghmouch47@gmail.com>
2026-09-09 20:27:10 +0100

    update the structers

---
+++ components/navBar.js
@@ -0,0 +1,26 @@
+'use strict'
+
+const navBar=document.createElement('div')
+
+ export function  navBar (){
+    
+    navBar.insertAdjacentHTML('afterbegin',`<div>
+
+            <img src="">
+
+            <div>
+            <a href="/Dashboard">Dashboard</a>
+            <a href="/Offres">offres</a>
+            <a href="/Rewards">rewards </a>
+            <a href="/Profile">profile</a>
+            </div>
+
+
+            </div>
+            `)}
+
+            
+
+
+
+    
---
+++ router/handler.js
@@ -0,0 +1,21 @@
+'use strict'
+
+import { navBar } from "../components/navBar"
+import Da
+const route = {
+    '/Dashboard': 'Dashboard',
+    '/Offeres': 'Offeres',
+    '/Rewards': 'Rewards',
+    '/Credit':'Credit',
+    '/Profile': 'Profile'
+
+}
+
+
+ const router = function () {
+
+
+
+}
+
+export default router
