ebba78c8855f4b9233375b400457e9936af56bf5
abderrahim ghmouch <abderrahimghmouch47@gmail.com>
2026-09-10 11:35:42 +0100

    create the routing handling mehtode

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
-import { router } from "./router/handler";
+import {
+    router
+} from "./router/handler.js";
+
+window.addEventListener('popstate', router);
 
 
+document.addEventListener('DOMContentLoaded', () => {
+
+    document.body.addEventListener('click', e => {
+
+        if (e.target.matches('a')) {
+
+            e.preventDefault()
+            window.history.pushState(null, null, e.target.href)
+
+            router()
+        }
+
+    })
+})
--- router/handler.js
+++ router/handler.js
@@ -1,21 +1,49 @@
 'use strict'
 
-import { navBar } from "../components/navBar"
-import Da
-const route = {
-    '/Dashboard': 'Dashboard',
-    '/Offeres': 'Offeres',
-    '/Rewards': 'Rewards',
-    '/Credit':'Credit',
-    '/Profile': 'Profile'
+import {  navBar} from "../components/navBar.js"
 
+import {Dashboard} from "../views/Dashboard.js"
+
+import {Login} from "../views/Login.js"
+
+import {Offeres} from "../views/Offeres.js"
+
+import {Signup} from "../views/Signup.js"
+
+import {Credit} from "../views/Credit.js"
+
+import {Rewards} from "../views/Rewards.js"
+
+
+
+const routes = {
+    '/': Dashboard,
+    '/Dashboard': Dashboard, 
+    '/login': Login,
+    '/Offres': Offeres,      
+    '/signup': Signup,
+    '/credit': Credit,
+    '/Rewards': Rewards
 }
 
+export const router = function () {
+    const root = document.querySelector('#root');
+    let path = window.location.pathname;
 
- const router = function () {
+    if (path === '/index.html') {
+        path = '/dashboard';
+    }
 
+    root.innerHTML = '';
 
+    root.insertAdjacentHTML('beforeend', navBar())
+
+    const views = routes[path]
+    if (!views) {
+        root.insertAdjacentElement('beforeend',`<div>404 error</div>`)
+    } else if (views) {
+
+       root.insertAdjacentHTML('beforeend', views());
+    }
 
 }
-
-export default router
